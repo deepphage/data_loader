636d92e995ff5197d2c550efc9b2ae81d259e4c7
PierpaoloLucarelli <plucarelli.94@gmail.com>
2019-05-25 19:27:26 +0200

    fixed loading files bug

--- data_loader.py
+++ data_loader.py
@@ -60,8 +60,10 @@ class PhageLoader():
 		return selected_files
 
 	def get_data_loader(self, n_files='all' ,n='all', read_length=100, batch_size=32, k=3, stride=1, embedding=None, embed_size=None, drop_last=False):
-		datasets = np.empty(len(self.filenames), dtype=object)
-		for i in range(len(self.filenames)):
+		if(n_files=='all'):
+			n_files = len(self.filenames)
+		datasets = np.empty(n_files, dtype=object)
+		for i in range(n_files):
 			dl = PhageFileLoader(self.datafolder+self.filenames[i])
 			dataset = dl.get_n_loaders(n=n,read_length=read_length, batch_size=batch_size,k=k,stride=stride,embedding=embedding,embed_size=embed_size)
 			datasets[i] = dataset
@@ -70,8 +72,10 @@ class PhageLoader():
 
 
 	def get_data_set(self, n_files='all' ,n='all', read_length=100, batch_size=32, k=3, stride=1, embedding=None, embed_size=None, drop_last=False):
-		datasets = np.empty(len(self.filenames), dtype=object)
-		for i in range(len(self.filenames)):
+		if(n_files=='all'):
+			n_files = len(self.filenames)
+		datasets = np.empty(n_files,  dtype=object)
+		for i in range(n_files):
 			dl = PhageFileLoader(self.datafolder+self.filenames[i])
 			dataset = dl.get_n_loaders(n=n,read_length=read_length, batch_size=batch_size,k=k,stride=stride,embedding=embedding,embed_size=embed_size)
 			datasets[i] = dataset
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 
 loader = PhageLoader("data/")
-loader = loader.get_data_loader(n='all',read_length=100, batch_size=32, k=3, stride=1, embedding="dict", embed_size=None, drop_last=False)
-for i, data in enumerate(loader, 0):
-	X, y = data
-	# one X is one batch of size 32 in this case
-	# each element of the bacth is a sequence of length read_length
-	print(X[0]) # <- this is a sequence of length 100 
+loader = loader.get_data_set(n='all',read_length=100, batch_size=32, k=3, stride=1, embedding="dict", embed_size=None, drop_last=False)
+print(loader)
+# for i, data in enumerate(loader, 0):
+# 	X, y = data
+# 	# one X is one batch of size 32 in this case
+# 	# each element of the bacth is a sequence of length read_length
+# 	print(X[0]) # <- this is a sequence of length 100 
 
 
 
